ebf3f127ad4c7bf363bb72d5e4c21490b775eba8
Maxim <maks_umov1996@mail.ru>
2021-12-01 18:12:30 +0600

    js learning

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+/* let answer = confirm('Вам есть 18 лет?');
+if (answer == true) {
+    console.log('Да, мне есть 18 лет');
+} else {
+    console.log('Вам нельзя смотреть данный контент!');
+} */
+
+for(let i = 1; i < 8; i++ ) {
+    console.log(i);
+}
+
+function logging() {
+    console.log('Hello,world!');
+}
+
+logging();
