d3895edff8e4b5986f9ebc109a6b26af67e97d42
Maxim <maks_umov1996@mail.ru>
2021-12-02 21:10:49 +0600

    add slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,17 @@
-/* let answer = confirm('Вам есть 18 лет?');
-if (answer == true) {
-    console.log('Да, мне есть 18 лет');
-} else {
-    console.log('Вам нельзя смотреть данный контент!');
-} */
-
-for(let i = 1; i < 8; i++ ) {
-    console.log(i);
-}
-
-function logging() {
-    console.log('Hello,world!');
-}
-
-logging();
+$(document).ready(function () {
+    $('.carousel__inner').slick({
+        speed: 300,
+        /* adaptiveHeight: true, */
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/slider/prev_arrow.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/slider/next_arrow.png"></button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+});
